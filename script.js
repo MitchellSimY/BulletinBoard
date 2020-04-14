62c71dcbcb87d4f8ebb9d9283f0502511f08c43f
MitchellSimY <47762360+MitchellSimY@users.noreply.github.com>
2020-04-14 15:44:43 -0500

    Finished Project.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 
+//  Readying the document.
 $(document).ready(function () {
+    // Getting JSON link.
     $.getJSON("https://spreadsheets.google.com/feeds/list/1pG-K6QhY8kfJE4KXF3F9fR8NKmusL5nXhmVcs76H_hA/od6/public/values?alt=json", function (data) {
 
+        // I passed "Data" in the json link, and I made the feed.entry = sheetData.
         var sheetData = data.feed.entry;
+        
+        // counter variable.
         var i;
 
+        // ForLoop that'll cycle through google sheets JSON
         for (i = 0; i < sheetData.length; i++) {
+
+            // Within the jSON text, the columns are labeled as "gsx$[...]"
+            // So I went ahead and grabbed that information and stringified it so it became a string.
+            // Then I replaced any quotations that would make the text look weird.
             var title = JSON.stringify(data.feed.entry[i]["gsx$title"]["$t"]);
             title = title.replace(/\"/g, "");
 
@@ -18,14 +28,16 @@ $(document).ready(function () {
             var imageIcon = JSON.stringify(data.feed.entry[i]["gsx$imageicon"]["$t"]);
             imageIcon = imageIcon.replace(/\"/g, "");
             
+            // Passing the information into a function.
             var output = htmlOutput(title, content, alertType, imageIcon);
         };
 
         
 
-
+        // Display function that takes four parameters of texts and image.
         function htmlOutput(titleOfPost, contentOfPost, postAlertType, postImage) {
 
+            // HTML markup to output onto webpage.
             var bullitenOutputHTML = '<div class="card" style="width: 20rem;">' +
                 '<img class="card-img-top" src ="' + postImage + '" alt = "Card image cap" > ' +
                 '<div class="card-body"> ' +
@@ -33,17 +45,11 @@ $(document).ready(function () {
                 '<p class="card-text">' + contentOfPost + '</p>' +
                 '</div>' +
                 '<div class="card-footer text-muted">' + postAlertType;
-            //$(html).hide().appendTo("#dynamicBulletin").fadeIn(1000);
+            
+            // Locating the ID "dynamicBulletin", then hiding it, only to add it in again and fade it in for UX.
             $('#dynamicBulletin').hide().append(bullitenOutputHTML).fadeIn(1000);
             return;
         }
-
-
-        
-
-        // document.getElementById('dynamicBulletin').innerHTML += output;
-
-
     });
 });
 
